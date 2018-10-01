478651846d9f27e2e329840232114a034eedaceb
kin chan <kinluek@gmail.com>
2018-10-01 11:53:49 +0100

    render simple map from state positions

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -6,10 +6,11 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  Dimensions,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
-
+import PinchZoomView from 'react-native-pinch-zoom-view'
 import { MonoText } from '../components/StyledText';
 
 export default class MapScreen extends React.Component {
@@ -19,23 +20,137 @@ export default class MapScreen extends React.Component {
   };
 
   state = {
-    text: 'testing state'
+    positions: [{
+      position: 'absolute',
+      top: 40,
+      left: 40,
+      width: 10,
+      height: 10,
+      backgroundColor: 'blue',
+    },
+    {
+      position: 'absolute',
+      top: 200,
+      left: 100,
+      width: 10,
+      height: 10,
+      backgroundColor: 'blue',
+    }
+    ]
   }
 
   render() {
+
+    let positions = {}
+    this.state.positions.forEach((position, i) => {
+      positions[`mapItem${i}`] = position
+    })
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+      },
+      map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+        backgroundColor: 'grey',
+      },
+      developmentModeText: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+      },
+      contentContainer: {
+        paddingTop: 30,
+      },
+      welcomeContainer: {
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 20,
+      },
+      welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10,
+      },
+      getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+      },
+      homeScreenFilename: {
+        marginVertical: 7,
+      },
+      codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)',
+      },
+      codeHighlightContainer: {
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 3,
+        paddingHorizontal: 4,
+      },
+      getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
+      },
+      tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+          ios: {
+            shadowColor: 'black',
+            shadowOffset: { height: -3 },
+            shadowOpacity: 0.1,
+            shadowRadius: 3,
+          },
+          android: {
+            elevation: 20,
+          },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+      },
+      tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
+      },
+      navigationFilename: {
+        marginTop: 5,
+      },
+      helpContainer: {
+        marginTop: 15,
+        alignItems: 'center',
+      },
+      helpLink: {
+        paddingVertical: 15,
+      },
+      helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+      },
+      ...positions
+    });
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <Text onPress={this.changeState}>{this.state.text}</Text>
+          <PinchZoomView style={styles.map}>
+            {this.state.positions.map((position, i) => (
+              <View key={`mapItem${i}`} style={styles[`mapItem${i}`]}></View>
+            ))}
+          </PinchZoomView>
         </ScrollView>
       </View>
     );
-  }
-
-  changeState = () => {
-    this.setState({
-      text: 'state has changed'
-    })
   }
 
   _maybeRenderDevelopmentModeWarning() {
@@ -72,91 +187,4 @@ export default class MapScreen extends React.Component {
   };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+

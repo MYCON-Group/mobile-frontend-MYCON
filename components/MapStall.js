7ce3aea9b2864765a4b2d6fcebf2366f51a712e6
mattegerton <matthewjegerton@googlemail.com>
2018-10-10 16:47:13 +0100

    add update notify alert to map

--- components/MapStall.js
+++ components/MapStall.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import { TouchableOpacity, StyleSheet, View, Image, TouchableHighlight } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableHighlight,
+  Platform
+} from "react-native";
 window.navigator.userAgent = "react-native";
+import { Icon } from "expo";
 
 class MapStall extends Component {
   state = {
-    color: "blue",
+    updated: false,
     showInfo: false
   };
 
@@ -13,38 +20,76 @@ class MapStall extends Component {
 
     const updateStyle = StyleSheet.create({
       updatedColour: {
-        backgroundColor: this.state.color,
+        backgroundColor: "#2196F3",
         borderRadius: 25,
-        padding: 5
+        padding: 5,
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        elevation: 10
+      },
+      icons: {
+        color: "#fff",
+        height: 100,
+        width: 100
       }
     });
     styles = styles.concat(updateStyle.updatedColour);
     return (
-      <TouchableHighlight key={`key${id}`} style={styles} onPress={this.notified}>
-        <Image
-          style={{
-            height: 100,
-            width: 100,
-            alignSelf: "center",
-            flex: 1,
-            flexDirection: "row",
-            borderRadius: 50
-          }}
-          source={{ uri: logo.stall_logo }}
-        />
+      <TouchableHighlight
+        key={`key${id}`}
+        style={styles}
+        onPress={this.notified}
+      >
+        <View>
+          <Image
+            style={{
+              height: 100,
+              width: 100,
+              alignSelf: "center",
+              flex: 1,
+              flexDirection: "row",
+              borderRadius: 50
+            }}
+            source={{ uri: logo.stall_logo }}
+          />
+          {this.state.updated && (
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                alignSelf: "center",
+                position: "absolute",
+                backgroundColor: "#2ecc71",
+                width: 30,
+                height: 30,
+                top: 0,
+                left: 0,
+                borderRadius: 50
+              }}
+            >
+              <Icon.Ionicons
+                size={30}
+                name={Platform.OS === "ios" ? `ios-alert` : "md-alert"}
+                style={styles.icons}
+              />
+            </View>
+          )}
+        </View>
       </TouchableHighlight>
     );
   }
 
   componentDidMount() {
     this.props.socket.on(`stall${this.props.id}`, () =>
-      this.setState({ color: "green" })
+      this.setState({ updated: true })
     );
   }
 
   notified = () => {
     this.setState({
-      color: "blue",
+      updated: false,
       showInfo: true
     });
     this.props.navigation.navigate("ShowStall", {

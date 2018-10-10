c7aa37a9e903f7f37a67712d29d560ff2cbf58c8
mattegerton <matthewjegerton@googlemail.com>
2018-10-10 09:44:25 +0100

    style login page

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -7,7 +7,9 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
-  View
+  View,
+  Text,
+  Image
 } from "react-native";
 import AppNavigator from "../navigation/AppNavigator";
 
@@ -19,16 +21,22 @@ export default class Auth extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-        <TouchableOpacity style={styles.buttonContainer}>
-          <Button onPress={this.showLogin} title="Sign in" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonContainer}>
-          <Button
-            style={styles.buttonStyle}
-            onPress={this.showTheApp}
-            title="Show me the app!"
+        <View style={styles.innerContainer}>
+          <Image
+            source={require("../assets/images/logo_transparent.png")}
+            style={{ width: 300, height: 300 }}
           />
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.showLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.buttonHollow}
+            onPress={this.showTheApp}
+          >
+            <Text style={styles.buttonHollowText}>Show me the app!</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     );
   }
@@ -43,12 +51,53 @@ export default class Auth extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  button: {
     backgroundColor: "#fff",
-    marginTop: `50%`
+    width: "30%",
+    borderRadius: 25,
+    margin: 5,
+    height: 40,
+    width: 150,
+    width: 200,
+    justifyContent: "center",
+    borderColor: "#fff",
+    borderWidth: 1
   },
-  buttonContainer: {
-    padding: "2%"
+  buttonHollow: {
+    backgroundColor: "#0a7ddf",
+    width: "30%",
+    borderRadius: 25,
+    margin: 5,
+    width: 200,
+    height: 40,
+    justifyContent: "center",
+    borderColor: "#fff",
+    borderWidth: 1
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#0a7ddf",
+    padding: 10
+  },
+  buttonHollowText: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#fff",
+    padding: 10
+  },
+  container: {
+    backgroundColor: "#0a7ddf",
+    paddingTop: 100,
+    height: "100%",
+    width: "100%",
+    flex: 1,
+    position: "relative"
+  },
+  innerContainer: {
+    alignItems: "center",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center"
   }
 });
